31792f768468e4cebead58ccbf62babb96ca301d
Moon222189 <mpoonsas@gmail.com>
2025-11-06 03:19:22 +0000

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,18 @@
+async function sendMessage() {
+    const input = document.getElementById("userInput");
+    const message = input.value;
+    input.value = "";
 
+    const chatbox = document.getElementById("chatbox");
+    chatbox.innerHTML += `<p><b>You:</b> ${message}</p>`;
+
+    const response = await fetch("http://127.0.0.1:5000/chat", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({message})
+    });
+
+    const data = await response.json();
+    chatbox.innerHTML += `<p><b>MoonAI:</b> ${data.response}</p>`;
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
